7a442fb69419811fc8e50c82c27e9dec79b3cee8
Anant8002749 <ak8002749@gmail.com>
2020-05-28 22:15:14 +0530

    Created a view for home

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port=8000;
 // use express router
 app.use('/', require('./routes'));
 
-// set the view engine
+// set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
