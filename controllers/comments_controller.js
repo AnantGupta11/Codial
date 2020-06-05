b195f6ddb67c6d5a4a52647d379a094b55dc6f49
Anant8002749 <ak8002749@gmail.com>
2020-06-06 00:49:49 +0530

    Display comment and related User

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,7 +2,7 @@ const Comment= require('../models/comment');
 const Post=require('../models/post');
 
 module.exports.create=function(req,res){
-    Post.findById(req.body.postid, function(err,post){
+    Post.findById(req.body.post, function(err,post){
             if(err){
                 console.log("Error in finding post",err);
                 return res.redirect('back');
@@ -10,7 +10,7 @@ module.exports.create=function(req,res){
             if(post){
                 Comment.create({
                     content:req.body.content,
-                    post:req.body.postid,
+                    post:req.body.post,
                     user:req.user._id
                 }, function(err,comment){
                     //handle err
