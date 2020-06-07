47e73d635ea230ed86db406052456cd0ddc5d3d1
Anant8002749 <ak8002749@gmail.com>
2020-06-07 22:12:39 +0530

    Converting to ajax creating post

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -4,20 +4,23 @@ const LocalStrategy=require('passport-local').Strategy;
 const User =require('../models/user');
 // Authentication using passport
 passport.use(new LocalStrategy({
-        usernameField: 'email'
+        usernameField: 'email',
+        passReqToCallback :true
     },
-    function(email,password, done){
+    function(req,email,password, done){
         //find a user and establish the identity
         User.findOne({email:email}, function(err, user){
 
             // if err in finding User
             if(err){
-                console.log("Error in finding User----->passport");
+                // console.log("Error in finding User----->passport");
+                req.flash('error',err);
                 return done(err);
             }
             // if userName or Password are Incorrect
             if(!user || user.password!= password){
-                console.log('Invalid Username/Password');
+                // console.log('Invalid Username/Password');
+                req.flash('error', 'Invalid Username/Password');
                 return done(null, false);
             }
 
