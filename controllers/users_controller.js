113d6565f63c7d065cb335b70080041ac45e7285
Anant8002749 <ak8002749@gmail.com>
2020-06-08 14:43:51 +0530

    config Multer For User Avatar

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -24,7 +24,7 @@ module.exports.update = async function(req, res){
             let user = await User.findById(req.params.id);
             User.uploadedAvatar(req, res, function(err){
                 if (err) {console.log('*****Multer Error: ', err)}
-                
+                // console.log(req.file);          
                 user.name = req.body.name;
                 user.email = req.body.email;
 
