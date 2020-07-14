b9f4b2c7aa7d59a13bc53270d1443ca64cfbec85
Anant8002749 <ak8002749@gmail.com>
2020-07-14 15:22:09 +0530

    Added google strategy

--- config/passport-google-oath2-strategy.js
+++ config/passport-google-oath2-strategy.js
@@ -3,7 +3,7 @@ const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto= require('crypto');
 const User=require('../models/user');
 
-
+//tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
         clientID: "217187460365-mb77o0latfhod3mtof0bgregu5ctfno1.apps.googleusercontent.com",
         clientSecret: "eSjKgcpatsieAMFteC905sIt",
@@ -11,7 +11,34 @@ passport.use(new googleStrategy({
     },
 
     function(accesssToken, refreshToken, profile, done){
-        User.findOne({email:profile.emails[0].value})
+        //find a user
+        User.findOne({email:profile.emails[0].value}).exec(function(err, user){
+            if(err){
+                console.log("Error in google strstegy",err);
+                return;
+
+                console.log(profile);
+                if(user){
+                    //if found set user as request.user
+                    return done(null, user);
+                }else{
+                    //if not found create the user and set it req.user
+                    user.create({
+                        name: profile.displayName,
+                        email:profile.emails[0].value,
+                        password: crypto.randomBytes(20).toString('hex')
+                    },function(err, user){
+                        if(err){
+                            console.log("Error in Creating user google strstegy",err);
+                            return;
+                        }
+                        return done(null, user);
+                    })
+                }
+            }
+        })
     }
 
 ))
+
+module.exports=passport;
