bf6c350c29fe73b3a34885d7b254603f549c975a
Anant8002749 <ak8002749@gmail.com>
2020-07-20 20:52:18 +0530

    Created chatRoom

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,20 @@
-
 module.exports.chatSockets= function(socketServer){
+    let io=require('socket.io')(socketServer);
+
+    io.sockets.on('connection',function(socket){
+        
+        console.log('New Connection received', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('Socket Disconnected');
+        })
+        socket.on('join_room', function(data){
+            console.log('Joining request receive...',data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined',data);
+        })
+    });
 
 }
