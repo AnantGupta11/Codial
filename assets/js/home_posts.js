0c208744745285a4fada71add53ad9313a4c1018
Anant8002749 <ak8002749@gmail.com>
2020-07-20 08:29:23 +0530

    Added Likes

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,9 +13,14 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
-                    deletePost($('.delete-post-button', newPost));
+                    deletePost($(' .delete-post-button', newPost));
+
                     // call the create comment class
                     new PostComments(data.data.post._id);
+
+                    // CHANGE :: enable the functionality of the toggle like button on the new post
+                    new ToggleLike($(' .toggle-like-button', newPost));
+
                     new Noty({
                         theme: 'relax',
                         text: "Post published!",
@@ -24,6 +29,7 @@
                         timeout: 1500
                         
                     }).show();
+
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -34,6 +40,7 @@
 
     // method to create a post in DOM
     let newPostDom = function(post){
+        // CHANGE :: show the count of zero likes on this post
         return $(`<li id="post-${post._id}">
                     <p>
                         
@@ -46,6 +53,15 @@
                         <small>
                         ${ post.user.name }
                         </small>
+                        <br>
+                        <small>
+                            
+                                <a class="toggle-like-button" data-likes="0" href="/likes/toggle/?id=${post._id}&type=Post">
+                                    0 Likes
+                                </a>
+                            
+                        </small>
+
                     </p>
                     <div class="post-comments">
                         
@@ -67,8 +83,8 @@
     }
 
 
-    //method to delete a post from Dom
-    let deletePost= function(deleteLink){
+    // method to delete a post from DOM
+    let deletePost = function(deleteLink){
         $(deleteLink).click(function(e){
             e.preventDefault();
 
@@ -85,13 +101,15 @@
                         timeout: 1500
                         
                     }).show();
-                },
-                error: function(error){
+                },error: function(error){
                     console.log(error.responseText);
                 }
-            })
-        })
+            });
+
+        });
     }
+
+
 
 
 
