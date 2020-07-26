a806983be7a84cc12124f04d9aa1c45b7c608910
Anant8002749 <ak8002749@gmail.com>
2020-07-26 22:43:55 +0530

    css

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,33 @@
 const gulp = require('gulp');
 
-const sass=require('gulp-sass');
-const cssnano=require('gulp-cssnano');
-const rev=require('gulp-rev');
-const uglify=require('gulp-uglify-es').default;
-const imagemin=require('gulp-imagemin');
-const del=require('del');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
 
 
 gulp.task('css', function(done){
-    console.log('minifying css');
-    gulp.src('./assets/scss/**/*.scss')
+    console.log('minifying css...');
+    gulp.src('./assets/sass/**/*.scss')
     .pipe(sass())
     .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets.css'));
 
-        gulp.src('./assets/**/*.css')
+     gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(rev.manifest({
-        cwd:'public',
-        merge:true
+        cwd: 'public',
+        merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
     done();
+});
 
 
-})
 gulp.task('js', function(done){
     console.log('minifying js...');
      gulp.src('./assets/**/*.js')
